49fcd87c324cd5b7230bb244a5ca441f1ae7f298
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2021-01-18 11:12:06 +0100

    Pushing code

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -7,13 +7,22 @@ public class Snake : MonoBehaviour
 {
     private Vector2Int gridDir;
     private Vector2Int gridPos;
+    public List<Vector2Int> snakePrevPosList;
+
     [SerializeField]
-    private float moveRate;
+    private GameObject snakeTail;
+    public float moveRate;
+    public int snakeSize;
+
 
     private void Awake()
     {
         gridPos = new Vector2Int(10, 10);
         InvokeRepeating("SetDir", 0.1f, moveRate);
+        gridDir.x = 1;
+
+        snakePrevPosList = new List<Vector2Int>();
+
     }
 
     private void Update()
@@ -21,11 +30,32 @@ public class Snake : MonoBehaviour
         PlayerInputs();
     }
 
+    public void SnakeSize(int tail)
+    {
+        switch (tail)
+        {
+            case 1:
+                snakeSize--;
+                break;
+
+            default:
+                snakeSize++;
+                AddLength();
+                break;
+        }
+    }
+
+    private void AddLength()
+    {
+        Instantiate(snakeTail, new Vector3(gridPos.x, gridPos.y), Quaternion.identity, this.gameObject.transform);
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Apple"))
         {
             col.GetComponent<AppleBehaviour>().Die();
+            SnakeSize(0);
         }
         
 
@@ -61,7 +91,15 @@ public class Snake : MonoBehaviour
 
     private void SetDir()
     {
+        snakePrevPosList.Insert(0, gridPos);
+
         gridPos += gridDir;
+        
+        if(snakePrevPosList.Count >= snakeSize + 1)
+        {
+            snakePrevPosList.RemoveAt(snakePrevPosList.Count - 1);
+        }
+
         transform.position = new Vector3(gridPos.x, gridPos.y);
         transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridDir) - 90); //-90 is there because the Sprite is stolen and it's facing direction was not accounted for by the creator
     }
---
+++ Assets/Scripts/SnakeTail.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTail : MonoBehaviour
+{
+
+    //private GameObject snake;
+    //private Vector2Int tailGridPos;
+    //private Vector2Int tailGridDir;
+    //public int tailID;
+
+    //private void Awake()
+    //{
+    //    snake = this.gameObject.GetComponentInParent<Snake>().gameObject;
+    //    tailID = snake.GetComponentInParent<Snake>().snakeSize;
+    //    InvokeRepeating("MoveTail", 0.1f, snake.GetComponent<Snake>().moveRate);
+    //}
+
+    //private void MoveTail()
+    //{
+    //    tailGridDir = snake.GetComponent<Snake>().snakePrevPosList[tailID];
+
+    //    tailGridPos += tailGridDir;
+
+    //    transform.position = new Vector3(tailGridPos.x, tailGridPos.y);
+    //}
+}
